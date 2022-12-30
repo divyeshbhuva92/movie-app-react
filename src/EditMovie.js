f3e1e0a4c7f7c70e39679007e7960a31ec71e52a
divyeshbhuva92 <dev92bhuva@gmail.com>
2022-12-30 17:48:26 +0530

    edit movie function added

--- src/EditMovie.js
+++ src/EditMovie.js
@@ -1,8 +1,7 @@
 import { useState, useEffect } from "react";
-import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { API } from "./globle";
+import { EditMovieForm } from "./EditMovieForm";
 
 export function EditMovie() {
   const { id } = useParams();
@@ -19,85 +18,4 @@ export function EditMovie() {
   }, [id]);
 
   return movie ? <EditMovieForm movie={movie} /> : "Loading...";
-}
-
-function EditMovieForm({ movie }) {
-  const [name, setName] = useState(movie.name);
-  const [rating, setRating] = useState(movie.rating);
-  const [poster, setPoster] = useState(movie.poster);
-  const [summary, setSummary] = useState(movie.summary);
-  const [trailer, setTrailer] = useState(movie.trailer);
-  const navigate = useNavigate();
-
-  const editMovie = () => {
-    const updateMovies = {
-      name,
-      poster,
-      rating,
-      summary,
-      trailer,
-    };
-
-    fetch(`${API}/movies/${movie.id}`, {
-      method: "PUT",
-      body: JSON.stringify(updateMovies),
-      headers: { "content-type": "application/json" },
-    })
-      .then((res) => res.json())
-      .then(() => navigate("/movies"));
-  };
-
-  return (
-    <div className="add-movie-form">
-      <TextField
-        label="Name"
-        className="add-movie-input"
-        placeholder="Name"
-        size="small"
-        value={name}
-        onChange={(event) => setName(event.target.value)}
-      />
-      <TextField
-        label="Rating"
-        className="add-movie-input"
-        placeholder="Rating"
-        size="small"
-        value={rating}
-        onChange={(event) => setRating(event.target.value)}
-      />
-      <TextField
-        label="Poster URL"
-        className="add-movie-input"
-        placeholder="Poster URL"
-        size="small"
-        value={poster}
-        onChange={(event) => setPoster(event.target.value)}
-      />
-      <TextField
-        label="Summary"
-        className="add-movie-input"
-        placeholder="Summary"
-        size="small"
-        value={summary}
-        onChange={(event) => setSummary(event.target.value)}
-      />
-      <TextField
-        label="Trailer URL"
-        className="add-movie-input"
-        placeholder="Trailer"
-        size="small"
-        value={trailer}
-        onChange={(event) => setTrailer(event.target.value)}
-      />
-
-      <Button
-        className="add-movie-btn"
-        variant="contained"
-        size="small"
-        onClick={editMovie}
-      >
-        Save
-      </Button>
-    </div>
-  );
 }
---
+++ src/EditMovieForm.js
@@ -0,0 +1,86 @@
+import { useState } from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import { useNavigate } from "react-router-dom";
+import { API } from "./globle";
+
+export function EditMovieForm({ movie }) {
+  const [name, setName] = useState(movie.name);
+  const [rating, setRating] = useState(movie.rating);
+  const [poster, setPoster] = useState(movie.poster);
+  const [summary, setSummary] = useState(movie.summary);
+  const [trailer, setTrailer] = useState(movie.trailer);
+  const navigate = useNavigate();
+
+  const editMovie = () => {
+    const updateMovies = {
+      name,
+      poster,
+      rating,
+      summary,
+      trailer,
+    };
+
+    fetch(`${API}/movies/${movie.id}`, {
+      method: "PUT",
+      body: JSON.stringify(updateMovies),
+      headers: { "content-type": "application/json" },
+    })
+      .then((res) => res.json())
+      .then(() => navigate("/movies"));
+  };
+
+  return (
+    <div className="add-movie-form">
+      <TextField
+        label="Name"
+        className="add-movie-input"
+        placeholder="Name"
+        size="small"
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+      />
+      <TextField
+        label="Rating"
+        className="add-movie-input"
+        placeholder="Rating"
+        size="small"
+        value={rating}
+        onChange={(event) => setRating(event.target.value)}
+      />
+      <TextField
+        label="Poster URL"
+        className="add-movie-input"
+        placeholder="Poster URL"
+        size="small"
+        value={poster}
+        onChange={(event) => setPoster(event.target.value)}
+      />
+      <TextField
+        label="Summary"
+        className="add-movie-input"
+        placeholder="Summary"
+        size="small"
+        value={summary}
+        onChange={(event) => setSummary(event.target.value)}
+      />
+      <TextField
+        label="Trailer URL"
+        className="add-movie-input"
+        placeholder="Trailer"
+        size="small"
+        value={trailer}
+        onChange={(event) => setTrailer(event.target.value)}
+      />
+
+      <Button
+        className="add-movie-btn"
+        variant="contained"
+        size="small"
+        onClick={editMovie}
+      >
+        Save
+      </Button>
+    </div>
+  );
+}
